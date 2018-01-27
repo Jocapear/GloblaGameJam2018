e01aa92fdc1d6224ce82d4e953d49d7de4786c7f
J97cp <juan97_carlos@hotmail.com>
2018-01-27 12:51:02 -0600

    Code & stuff
    Finally

---
+++ Assets/Scripts/SignalScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalScript : MonoBehaviour {
+	public bool signalType;
+	public float destroyTime;
+
+	void Start(){
+		Destroy(gameObject,destroyTime);
+	}
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag != "MainCamera"){
+			Destroy(gameObject);
+		}
+		DispenserScript target = c.transform.GetComponent<DispenserScript>();
+		if (target != null){
+			target.EnterSignal(signalType);
+		}
+
+		
+	}
+}
